07090fe9e5b0f41d08141ea194ca1d1ce5a23ba3
Dylan <johnsondylang@gmail.com>
2019-11-02 15:29:01 -0700

    fixed bug in depth first

--- src/algorithms/depthFirst.js
+++ src/algorithms/depthFirst.js
@@ -1,14 +1,21 @@
 
-export async function depthFirst(grid) {
+/**
+ * Applies the depth-first search 
+ * @param {pathfinder-grid} grid - The pathfinder grid to search  
+ * @param {Array<grid-cell>} alternateStart - Path to use as the start instead of the grid.startCell. Used to recurse back from dead ends
+ */
+export async function depthFirst(grid, alternateStart) {
      
     // the queue will store the paths taken as Arrays
     let stack = [];
+
+    // if an alternate path was provided, will use that as the inital path to explore
+    let start = alternateStart ? alternateStart : [grid.startCell];
     
     // the first 'path' that will be searched is the start node by itself
-    stack.push([grid.startCell]);
+    stack.push(start);
 
-    // 
-    let index = 0;
+    // go until the stack is empty or start node is found
     while (stack.length > 0) {
         // remove the last path out of the queue 
         let lastPath = stack.pop(); 
@@ -20,13 +27,25 @@ export async function depthFirst(grid) {
         // ignore cells that are 'blocked' as those are considered walls or obsticals to go around
         const adjacentCells = grid.getAdjacentCells(lastCell, {ignoreBlocked:true, ignoreChecked:true})
         
+        // Search hits a dead end when there are no adjacentCells to explore and the stack is empty
+        if (adjacentCells.length === 0 && stack.length === 0) {
+            // remove the last cell (this is the dead end) from the last path explored
+            lastPath.pop();
+            
+            // if there are no cells left in the lastPath than there is nothing left to search - there are no valid paths to the end
+            if (lastPath.length === 0) return [];
+            
+            // recursively call the depthFirst search again to backtrack to last cell with valid branch
+            return await depthFirst(grid, lastPath);
+        }
+
         // create a new path for each valid adjacent cell by extending the last path with that cell
         // adding that new path into the queue
         for (const cell of adjacentCells) {
             // if the cell is the endCell, we have found the optimal path
             if (cell.column == grid.endCell.column && cell.row == grid.endCell.row) {
                 // return the path that led to the find
-                return lastPath.concat([grid.endCell])                    
+                return lastPath.concat([grid.endCell]);                
             }
 
             // highlight the newly checked cell, using await to since the highlight function takes into account speed levels to process render
@@ -36,6 +55,10 @@ export async function depthFirst(grid) {
             const newPath = lastPath.concat([cell]);
             // push that new path into the queue
             stack.push(newPath); 
+
+            // do not evaluate all the adjacent cells in depth first.
+            // break here to coninue exploring in one direction
+            break;
         };
     }
 }
